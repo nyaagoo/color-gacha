f1697e5bb72726ea96e37e2554cf9ecbef4fa298
nyagoo <acc.mg47@gmail.com>
2019-04-13 16:50:36 +0900

    [Update] Cloud Functions API 追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,73 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as admin from "firebase-admin";
+import { Request, Response, NextFunction } from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("../dev/color-gacha-firebase-adminsdk-sy9f7-80bc62164e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://color-gacha.firebaseio.com"
+});
+// import * as cors from "cors";
+
+const app = express();
+
+app.get("/users", validate, aa);
+app.get("/users/b", aa);
+app
+  .route("/book")
+  .get(function(req, res) {
+    res.send("Get a random book");
+  })
+  .post(function(req, res) {
+    res.send("Add a book");
+  })
+  .put(function(req, res) {
+    res.send("Update the book");
+  });
+
+app.get("/gacha", validate, Gacha);
+export function validate(req: Request, res: Response, next: NextFunction) {
+  // eslint-disable-next-line no-console
+  console.log("Validation");
+  next();
+}
+export async function Gacha(req: Request, res: Response) {
+  const colorList: Color[] = [];
+  await admin
+    .firestore()
+    .collection("colors")
+    .get()
+    .then(async function(querySnapshot) {
+      await querySnapshot.forEach(async function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        await colorList.push({
+          id: doc.data().id,
+          name: doc.data().name,
+          code: doc.data().code
+        });
+      });
+    });
+
+  res.send(colorList);
+}
+
+export interface Color {
+  id: number;
+  name: string;
+  code: string;
+}
+
+export async function aa(req: Request, res: Response) {
+  const users = [{ id: 1, name: "sss" }];
+  // データを返却
+  res.send(JSON.stringify(users));
+}
+
+app.get("/users/a", [], (req: Request, res: Response) => {
+  const users = [{ id: 1, name: "dfs" }];
+  res.send(JSON.stringify(users));
+});
+
+exports.app = functions.https.onRequest(app);
